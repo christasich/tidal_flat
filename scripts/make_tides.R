7f8e7610de7352d25d1a5c199932263993616897
Chris Tasich <chris.tasich@gmail.com>
2020-07-17 17:51:26 -0500

    backed up local repository, deleted local, recloned from GitHub, then copied back up into new local repo

--- scripts/make_tides.R
+++ scripts/make_tides.R
@@ -1,4 +1,3 @@
-#library.path <- .libPaths("/home/chris/R/x86_64-pc-linux-gnu-library/3.6")
 library(tsibble)
 library(lubridate)
 library(tidyverse)
@@ -43,6 +42,6 @@ tides$pressure = predict(mod, newdata=index)
 
 tides$pressure = tides$pressure + sl_vec
 
-write_feather(tides,sprintf('../../interim/tides/tides-yr_%.0f-dt_%.0f-slr_%.4f.feather', run_length, as.numeric(as.duration(dt), 'minutes'), slr))
+write_feather(tides,sprintf('../../interim/tides/tides-yr_%.0f-dt_%.0f-slr_%.4f.feather', run_length, as.numeric(as.duration(dt), 'seconds'), slr))
 
 print('Tides made')
