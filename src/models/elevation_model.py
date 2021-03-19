d1a36442c16c9e746f6a72c050d0ba5b4f3ee426
Chris Tasich <chris.tasich@gmail.com>
2021-03-19 23:51:35 +0000

    Added organic, compaction, and subsidence.

--- src/models/elevation_model.py
+++ src/models/elevation_model.py
@@ -16,6 +16,18 @@ def stokes_settling(grain_dia, grain_den, fluid_den = 1000.0, fluid_visc = 0.001
     return settle_rate
 
 
+def degrade_linear(init_elev, time_arr, comp_rate, sub_rate):
+    """
+    Function that linearly degrades land-surface elevation given an initial
+    elevation, a time array of lenght N, compaction rate, and subsidence rate.
+    Returns an array of elevations of length N.
+    """
+    comp_and_sub = time_arr * (comp_rate + sub_rate)
+    elevs = init_elev - comp_and_sub
+
+    return elevs
+
+
 class ResultClass:
     """
     Result class to imitate scipy.integrate.solve_ivp result for consistency
@@ -41,7 +53,7 @@ def concatenate_results(results, new_result):
     return ResultClass(times, [concs, elevs])
 
 
-def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0, compaction_rate=0.0, subsidence_rate=0.0, init_elev=0.0, init_conc=0.0, timestep=1.0, min_depth=0.001):
+def simulate_elevation(water_heights, settle_rate, bulk_dens, bound_conc, org_rate=0.0, comp_rate=0.0, sub_rate=0.0, init_elev=0.0, init_conc=0.0, timestep=1.0, min_depth=0.001):
     '''
     Zero-dimensional model of elevation change on a tidal platform given an array of water heights, settling rate of the sediment,
     dry bulk density of the sediment, and boundary concentration of the tidal channel.
@@ -58,7 +70,7 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
     below_platform.direction = -1
 
     # Function to solve derivatives of concentration and elevation.
-    def solve_odes(t, init_vals, *args):
+    def aggrade(t, init_vals, *args):
 
         # set initial values for concentration and elevation
         init_conc = init_vals[0]
@@ -77,7 +89,8 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
             H = 0
 
         delta_conc = - (settle_rate * init_conc) / depth - H / depth * (init_conc - bound_conc) * tide_deriv
-        delta_elev = settle_rate * (init_conc + delta_conc) / bulk_dens + organic_rate - compaction_rate - subsidence_rate
+        sed_rate = settle_rate * (init_conc + delta_conc) / bulk_dens
+        delta_elev = sed_rate + org_rate - comp_rate - sub_rate
 
         return [delta_conc, delta_elev]
 
@@ -85,16 +98,24 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
 
     # Make timeseries of water heights
     try:  # Try to infer timestep.
-        pd.infer_freq(water_heights.index)
+        water_heights.index.freq.n
+        #pd.infer_freq(water_heights.index)
     except:  # If cannot, use timestep passed by function. 1s is the default.
         vals = water_heights
         print('Couldn\'t infer timestep. Using {}s timestep.'.format(timestep))
     else:  # Set timestep to infered value.
-        timestep = pd.tseries.frequencies.to_offset(pd.infer_freq(water_heights.index)).n
+        #timestep = pd.tseries.frequencies.to_offset(pd.infer_freq(water_heights.index)).n
+        timestep = water_heights.index.freq.n
         vals = water_heights.values
 
     index = np.arange(0, len(water_heights) * timestep, timestep)  # make numeric index in seconds using timestep
     data = pd.Series(data=vals, index=index)  # make pd.Series
+
+    # Convert yearly linear rates to per timestep
+
+    org_rate = org_rate / 365 / 24 / 60 / 60 * timestep
+    comp_rate = comp_rate / 365 / 24 / 60 / 60 * timestep
+    sub_rate = sub_rate / 365 / 24 / 60 / 60 * timestep
 
     # Initialize
     elev = init_elev
@@ -102,15 +123,23 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
     pos = 0
     results = ResultClass()
 
-    # Progress through timeseries until all data is processed or an error is raised.
+    # Progress through timeseries until all data is processed or an error is raised
     while True:
         remaining_data = data.loc[pos:]  # remaining tide data to be processed
-        data_above_platform = remaining_data[remaining_data > (elev + min_depth)]  # tide data that is above the platform
+        elev_arr = degrade_linear(init_elev=elev,
+                                  time_arr=(remaining_data.index.values - remaining_data.index.values[0]),
+                                  comp_rate=comp_rate, sub_rate=sub_rate)
+        elev_ser = pd.Series(data=elev_arr, index=remaining_data.index)
+        data_above_platform = remaining_data[remaining_data > (elev_ser + min_depth)]  # tide data that is above the platform
 
         # break when not enough data to make a spline
         if len(data_above_platform) < 4:
+            results.t = np.append(results.t, data.index.values[-1])
+            results.y[0] = np.append(results.y[0], 0)
+            results.y[1] = np.append(results.y[1], elev_ser.values[-1])
             break
 
+        pos_at_start = data_above_platform.index[0] # index value of start of next inundation
         pos_at_ends = data_above_platform.index[np.where(np.diff(data_above_platform.index) != timestep)]  # index values of end of remaining inundations
 
         # set end position of integration
@@ -121,9 +150,12 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
 
         # define subset to integrate
         subset = data_above_platform.loc[:end]
+        elev = elev_ser.loc[pos_at_start]
 
         # ensure subset is long enough to make a spline
         if len(subset) < 4:
+            pos = subset.index[-1] + 1
+            elev = elev_ser.loc[pos]
             continue
 
         # create continuous functions for tide height and the derivative of tide height to be called by the solver
@@ -134,62 +166,66 @@ def aggrade(water_heights, settle_rate, bulk_dens, bound_conc, organic_rate=0.0,
         t_span = [subset.index[0], subset.index[-1]]
 
         # integrate over the current inundation cycle
-        result = solve_ivp(fun=solve_odes, t_span=t_span, y0=[conc, elev],
-                           events=below_platform, args=(bound_conc, settle_rate, bulk_dens, min_depth, organic_rate, compaction_rate, subsidence_rate))
+        aggrade_result = solve_ivp(fun=aggrade, t_span=t_span, y0=[conc, elev],
+                           events=below_platform, args=(bound_conc, settle_rate, bulk_dens, min_depth, org_rate, comp_rate, sub_rate))
 
         # raise exception if solver fails
-        if result.status == -1:
+        if aggrade_result.status == -1:
             print('Solver failed to integrate from t={} to t={}.'.format(t_span[0], t_span[-1]))
-            print(result.message)
+            print(aggrade_result.message)
             break
 
         # concatenate data
-        results = concatenate_results(results, result)
+        results = concatenate_results(results, aggrade_result)
 
         # update params for next run
-        elev = result.y[1][-1]
-        pos = subset.index[-1] + 1
+        pos = aggrade_result.t[-1] + 1
+        elev = aggrade_result.y[1][-1]
+
+    # add start of elevation
+    if results.t[0] != 0:
+        results.t = np.insert(results.t, 0, 0)
+        results.y[0] = np.insert(results.y[0], 0, 0)
+        results.y[1] = np.insert(results.y[1], 0, init_elev)
 
     return results
 
 
-def run_model(tides_ts, settle_rate, bulk_dens, bound_conc, organic_rate=0.0, compaction_rate=0.0, subsidence_rate=0.0, init_elev=0.0, years=1, slr=0, verbose=False):
+def run_model(tides_ts, settle_rate, bulk_dens, bound_conc, org_rate=0.0, comp_rate=0.0, sub_rate=0.0, init_elev=0.0, years=1, slr=0, verbose=False):
     '''
-    Wrapper for the aggrade function that simulates multiple years of tidal inundation
+    Wrapper for the run_one function that simulates multiple years of tidal inundation
     with linear sea level rise. Returns a pd.Series for the tides and a solve_ivp
     result-like object for the integration.
     '''
     start_time = time.perf_counter()
     # reindex input tides to use pd.timedelta_range instead of actual dates
-    tides_reindex = pd.timedelta_range(start=0, periods=len(tides_ts), freq=tides_ts.index.freq)
-    tides = pd.Series(data=tides_ts.values, index=tides_reindex)
-    data = tides
+    index = pd.timedelta_range(start=0, periods=len(tides_ts), freq=tides_ts.index.freq)
+    data = pd.Series(data=tides_ts.values, index=index)
 
     # initialize
     timestep = tides_ts.index.freq.n
     elev = init_elev
     results = ResultClass()
-    tides = pd.Series()
+    tides = pd.Series(dtype='float64')
 
     # loop through a specified number of years
     for year in range(0, years):
         offset = (len(data) * timestep) * year  # offset to be add to tide index and result times depending on the year
 
         # make pd.Series for current year of tide data
-        data_index = pd.timedelta_range(start=pd.offsets.Second(offset), periods=len(data), freq=data.index.freq)
-        data = pd.Series(data=data.values, index=data_index)
+        data = pd.Series(data=data.values, index=index + pd.offsets.Second(offset))
 
         # run model for one year and update time vector with offset
-        result = aggrade(water_heights=data, settle_rate=settle_rate, bulk_dens=bulk_dens, bound_conc=bound_conc, organic_rate=organic_rate, compaction_rate=compaction_rate, subsidence_rate=subsidence_rate, init_elev=elev)
+        result = simulate_elevation(water_heights=data, settle_rate=settle_rate, bulk_dens=bulk_dens, bound_conc=bound_conc, org_rate=org_rate, comp_rate=comp_rate, sub_rate=sub_rate, init_elev=elev)
         result = ResultClass(result.t + offset, result.y)
-
-        # update params for next run
-        elev = result.y[1][-1]
-        data = data + slr
 
         # concatenate results
         results = concatenate_results(results, result)
         tides = tides.append(data)
+        
+        # update params for next year
+        elev = result.y[1][-1]
+        data = data + slr
 
     if verbose is True:
         print('Simulation length:     {} yr'.format(years))
