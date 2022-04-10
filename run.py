6005648781111c9418c22e328ebc73b3dd4dfaa7
Chris Tasich <chris.tasich@gmail.com>
2022-04-09 19:41:11 -0500

    updated args.end in run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import argparse
 import tidal_marsh as tm
-from loguru import logger
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -8,9 +7,12 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--start', type=int)
     parser.add_argument('-e', '--end', type=int)
     args = parser.parse_args()
-    logger.info(f'Running simulations for RUN# {args.start:04} to {args.end:04}.')
-    ids = range(args.start, args.end+1)
 
     sim = tm.simulation.Simulations(config_path=args.config)
+
+    if not args.end:
+        args.end = sim.metadata.index[-1]
+
+    ids = range(args.start, args.end+1)
     sim.setup()
     sim.run(ids=ids)
